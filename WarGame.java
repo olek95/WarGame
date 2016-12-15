20315fa86160fed8b3e49191ca3b388da2588762
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-12-16 00:17:01 +0100

    Create WarGame.java

---
+++ WarGame.java
@@ -0,0 +1,24 @@
+package wargame;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class WarGame extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("WarGameFXML.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
