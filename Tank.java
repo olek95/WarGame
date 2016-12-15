1379ad8c03f08f99a3d6dcde352225562e76df09
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-12-16 00:16:09 +0100

    Create Tank.java

---
+++ Tank.java
@@ -0,0 +1,13 @@
+package wargame;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Tank extends MilitaryUnit{
+    public Tank(){
+        this.img = new ImageView(new Image("file:images/tank.png"));
+        this.attack = 15;
+        this.life = 30;
+        this.speed = 10;
+    }
+}
