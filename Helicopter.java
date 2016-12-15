058a17ffb2d64f64a1b47d6b0dfcfe5ed4b394cc
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-12-16 00:16:22 +0100

    Create Helicopter.java

---
+++ Helicopter.java
@@ -0,0 +1,13 @@
+package wargame;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Helicopter extends MilitaryUnit{
+    public Helicopter(){
+        this.img = new ImageView(new Image("file:images/helicopter.png"));
+        this.attack = 10;
+        this.life = 20;
+        this.speed = 15;
+    }
+}
