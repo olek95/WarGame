ab799f2b7b1652d0e0902be9ab68a88e638a4d6b
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-12-16 15:58:46 +0100

    Update WarGameFXMLController.java
    dodanie pobierania marginesu, dodanie pobierania rozmiaru pola bitwy, pobranie kontrolera, pobranie lokacji kliknięcia myszy, testy

--- WarGameFXMLController.java
+++ WarGameFXMLController.java
@@ -3,6 +3,8 @@ package wargame;
 import java.awt.event.MouseAdapter;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
 import javafx.event.ActionEvent;
@@ -11,6 +13,7 @@ import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.PickResult;
@@ -18,12 +21,15 @@ import javafx.scene.layout.Pane;
 
 public class WarGameFXMLController implements Initializable {
     private static double BATTLEFIELD_WIDTH, MARGIN;
-    private double click;
+    private static WarGameFXMLController controller;
     private MilitaryUnit unit;
+    private Player p;
     @FXML
-    private Button buySoldierButton;
+    private Button buySoldierButton, exitButton;
     @FXML
     private Pane battlefieldPane;
+    @FXML
+    private TextArea battleTextArea;
     @FXML
     private void buySoldier(ActionEvent event) throws InterruptedException {
         unit = MilitaryUnitFactory.createMilitaryUnit(MilitaryUnitType.SOLDIER);
@@ -32,21 +38,54 @@ public class WarGameFXMLController implements Initializable {
     @Override
     @FXML
     public void initialize(URL url, ResourceBundle rb) {
+        controller = this;
+        p = new Player();
         BATTLEFIELD_WIDTH = battlefieldPane.getPrefWidth();
         MARGIN = 165;
+       MilitaryUnit u = MilitaryUnitFactory.createMilitaryUnit(MilitaryUnitType.SOLDIER);
+        u.getImg().setLayoutX(BATTLEFIELD_WIDTH - u.getImg().getImage().getWidth());
+        u.getImg().setLayoutY(0);
+        battlefieldPane.getChildren().add(u.getImg());
+        new Thread(u).start();
+        //try {
+        //    Thread.sleep(1000);
+        //} catch (InterruptedException ex) {
+        //    Logger.getLogger(WarGameFXMLController.class.getName()).log(Level.SEVERE, null, ex);
+        //}
+          MilitaryUnit u2 = MilitaryUnitFactory.createMilitaryUnit(MilitaryUnitType.SOLDIER);
+        u2.getImg().setLayoutX(BATTLEFIELD_WIDTH - u2.getImg().getImage().getWidth());
+        u2.getImg().setLayoutY(97);
+         battlefieldPane.getChildren().add(u2.getImg());
+         new Thread(u2).start();
         battlefieldPane.setOnMouseClicked(event -> {
-            click = event.getY();
             battlefieldPane.getChildren().add(unit.getImg());
+            p.addUnit(unit);
+            unit.getImg().setLayoutY(getLocationY(event.getY()));
             new Thread(unit).start();
         });
+        exitButton.setOnAction(value -> {
+            System.exit(0);
+        });
     }    
-    public double getRow(){
-        return click;
+    public double getLocationY(double click){
+        if(click < 97) return 0; // 97 to wysokość najwyższego obrazka 
+        else if(click > 97 && click < 194) return 97;
+        else if(click > 194 && click < 291) return 194;
+        return 291;
     }
     public static double getBattlefieldWidth(){
         return BATTLEFIELD_WIDTH;
     }
     public static double getMargin(){
         return MARGIN;
+    }
+    public static WarGameFXMLController getController(){
+        return controller;
+    }
+    public Pane getBattlefieldPane(){
+        return battlefieldPane;
+    }
+    public TextArea getBattleTextArea(){
+        return battleTextArea;
     }
 }
