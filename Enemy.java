653e50ac7f5be0ac9de3342d0edca3f86d190b83
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-12-18 14:16:02 +0100

    Create Enemy.java

---
+++ Enemy.java
@@ -0,0 +1,56 @@
+package wargame;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
+import javafx.concurrent.Task;
+import javafx.scene.image.ImageView;
+
+public class Enemy extends Task{
+    private static ArrayList<MilitaryUnit> units = new ArrayList();
+    public Enemy(){
+        units = new ArrayList();
+        units.add(MilitaryUnitFactory.createMilitaryUnit(MilitaryUnitType.BASE));
+    }
+    protected Void call(){
+        Random rand = new Random();
+        for(int i = 0; i < 10; i++){
+            Platform.runLater(() -> {
+                MilitaryUnit u = MilitaryUnitFactory.createMilitaryUnit(MilitaryUnitType.SOLDIER);
+                WarGameFXMLController.getController().getBattlefieldPane().getChildren().add(u.getImg());
+                units.add(u);
+                u.getImg().setLayoutX(WarGameFXMLController.getBattlefieldWidth()  - u.getImg().getImage().getWidth());
+                u.getImg().setLayoutY(randPosition());
+                new Thread(u).start();
+            });
+        try{
+            Thread.sleep(rand.nextInt(15001) + 5000); // zakładam że komputer będzie wysyłał jednostki z odstępami czasowymi od 5000 do 20000 milisekund
+        }catch(InterruptedException e){
+                Logger.getLogger(WarGameFXMLController.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }
+         return null;
+    }
+    public static MilitaryUnit getUnit(ImageView img){
+        for(MilitaryUnit unit : units)
+            if(unit.getImg().equals(img)) return unit; 
+        return null;
+    }
+    public static void remove(MilitaryUnit unit){
+        units.remove(unit);
+    }
+    public static int size(){
+        return units.size();
+    }
+    public static MilitaryUnit getBase(){
+        System.out.println("WSZEDLEM" + units.size());
+        return units.get(0);
+    }
+    private static int randPosition(){
+        Random rand = new Random();
+        int[] positions = {0, 97, 194, 291};
+        return positions[rand.nextInt(4)];
+    }
+}
