74c1bbee287384eb2815aeae55bab626f8ece203
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-12-18 14:16:26 +0100

    Update MilitaryUnit.java

--- MilitaryUnit.java
+++ MilitaryUnit.java
@@ -1,41 +1,56 @@
 package wargame;
 
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
 import javafx.geometry.Bounds;
 import javafx.scene.Node;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-public abstract class MilitaryUnit implements Runnable{
+public abstract class MilitaryUnit extends Task{
     protected ImageView img;
-    protected int attack, life, speed;
-    private boolean isPlayerUnit;
+    protected int attack, life, initialLife, speed, price;
+    private boolean isPlayerUnit, arrived;
+    private GameManager manager = new GameManager();
     protected MilitaryUnitType type;
-    public void run(){
+    public Void call(){
         if(Player.getUnits().contains(this)) isPlayerUnit = true;
         do{
             try {
-                if(isPlayerUnit) img.setLayoutX(speed + img.getLayoutX());
-                else img.setLayoutX(img.getLayoutX() - speed);
+                arrived = !((isPlayerUnit && img.getLayoutX() + img.getImage().getWidth() < WarGameFXMLController.getBattlefieldWidth() - WarGameFXMLController.getMargin())
+                || (!isPlayerUnit && img.getLayoutX() > WarGameFXMLController.getMargin()));
+                if(!arrived){
+                    if(isPlayerUnit) img.setLayoutX(speed + img.getLayoutX());
+                    else img.setLayoutX(img.getLayoutX() - speed);
+                }
                 ImageView neighbour;
                 synchronized(this){
                     do{
                         wait(100);
                         neighbour = stop();
+                        for(Node n : WarGameFXMLController.getController().getBattlefieldPane().getChildren()) System.out.print(((ImageView)n).getImage() + " ");
+                        System.out.println(isPlayerUnit + " " + img.getImage() + " " + neighbour + " " + life);
                         if(isPlayerUnit && neighbour != null && !Player.hasImageView(neighbour)){
+                            wait(200);
                             attack(neighbour);
+                        }else{
+                            if(arrived && isPlayerUnit && neighbour == null){
+                                wait(200);
+                                attack(null);
+                            }
                         }
                     }while(neighbour != null);
                 }
             }catch(InterruptedException e){
                 Logger.getLogger(MilitaryUnit.class.getName()).log(Level.SEVERE, null, e);
             }
-            System.out.println((!isPlayerUnit && img.getLayoutX() > WarGameFXMLController.getMargin()));
-        }while((isPlayerUnit && img.getLayoutX() + img.getImage().getWidth() < WarGameFXMLController.getBattlefieldWidth() - WarGameFXMLController.getMargin())
-                || (!isPlayerUnit && img.getLayoutX() > WarGameFXMLController.getMargin()));
+        }while(life > 0);
+        return null;
     }
     public ImageView getImg(){
         return img;
@@ -67,6 +82,36 @@ public abstract class MilitaryUnit implements Runnable{
         String t1 = text.substring(0, i + 2); // +2 bo chcę objąć nr wiersza wraz z kropką za nim
         String t2 = text.substring(i);
         t2 = t2.substring(t2.indexOf("\n"));
-        battleTextArea.setText(t1 + type.name() + t2);
+        MilitaryUnit enemy;
+        if(neighbour != null) enemy = Enemy.getUnit(neighbour);
+        else enemy = Enemy.getBase();
+        battleTextArea.textProperty().set(t1 + type.name() + "-" + enemy.type.name() + " " + life + "-" + enemy.life + t2);
+        Random rand = new Random(); 
+        if(rand.nextInt(2) == 0){
+            life -= enemy.attack; 
+            battleTextArea.textProperty().set(t1 + type.name() + "-" + enemy.type.name() + " " + life + "-" + enemy.life + t2);
+            if(life <= 0){
+                destroy(this);
+                Player.remove(this);
+                System.out.println("ZNISZCZONY");
+            }
+        }
+        if(life > 0 && rand.nextInt(2) == 0){
+            enemy.life -= attack;
+            battleTextArea.textProperty().set(t1 + type.name() + "-" + enemy.type.name() + " " + life + "-" + enemy.life + t2);
+            if(enemy.life <= 0){
+                manager.changePoints(enemy.initialLife);
+                destroy(enemy);
+                Enemy.remove(enemy);
+                enemy = null;
+                neighbour = null;
+                System.out.println("ZNISZCZONY");
+            }
+        }
+    }
+    public void destroy(MilitaryUnit unit){
+        unit.img.setImage(null);
+        unit.img = null;
+        unit = null;
     }
 }
