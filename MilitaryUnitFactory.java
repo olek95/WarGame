9c31afe69570553ef07f8988345ba4e3d615a1ee
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-12-18 14:20:07 +0100

    Update MilitaryUnitFactory.java
    skrocenie zapisu, dodanie bazy

--- MilitaryUnitFactory.java
+++ MilitaryUnitFactory.java
@@ -2,15 +2,10 @@ package wargame;
 
 public class MilitaryUnitFactory {
     public static MilitaryUnit createMilitaryUnit(MilitaryUnitType type){
-        if(MilitaryUnitType.SOLDIER.equals(type)){
-            return new Soldier(); 
-        }
-        if(MilitaryUnitType.TANK.equals(type)){
-            return new Tank();
-        }
-        if(MilitaryUnitType.HELICOPTER.equals(type)){
-            return new Helicopter();
-        }
+        if(MilitaryUnitType.SOLDIER.equals(type)) return new Soldier(); 
+        if(MilitaryUnitType.TANK.equals(type)) return new Tank();
+        if(MilitaryUnitType.HELICOPTER.equals(type)) return new Helicopter();
+        if(MilitaryUnitType.BASE.equals(type)) return new Base();
         return null;
     }
 }
