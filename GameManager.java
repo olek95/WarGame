f50525c09824257bb23af2dd36cbcce70578ee7e
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-12-19 00:05:18 +0100

    Update GameManager.java

--- GameManager.java
+++ GameManager.java
@@ -4,6 +4,7 @@ import javafx.scene.control.TextField;
 
 public class GameManager {
     private static GameManager manager;
+    private static boolean gameover = false;
     public static synchronized GameManager createGameManager(){
         if(manager == null) manager = new GameManager(); 
         return manager;
@@ -25,5 +26,17 @@ public class GameManager {
     public void changePoints(int points){
         TextField pointsTextField = WarGameFXMLController.getController().getPointsTextField();
         pointsTextField.textProperty().set((Integer.parseInt(pointsTextField.getText()) + points) + "");
+    }
+    public void setGameover(boolean value){
+        gameover = value;
+    }
+    public boolean getGameover(){
+        return gameover;
+    }
+    public void check(){
+        int i = 0; 
+        WarGameFXMLController.getController().getBattlefieldPane().getChildren().clear();
+        System.out.println("ILOSC NOD: " + WarGameFXMLController.getController().getBattlefieldPane().getChildren().size());
+        System.out.println("ILOSC PRZECIWNIKA: " + Enemy.size());
     }
 }
