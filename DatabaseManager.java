042a2e683e16f2f69a91918e17a252938e8ca8ee
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-12-20 21:01:14 +0100

    Update DatabaseManager.java
    sprawdzenie czy gracz istnieje w bazie, tworzenie menedżera bazy

--- DatabaseManager.java
+++ DatabaseManager.java
@@ -3,6 +3,7 @@ package wargame;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -14,6 +15,10 @@ public class DatabaseManager {
         Class.forName(jdbcDriver);
         conn = DriverManager.getConnection(url, user, password);
     }
+    public static synchronized DatabaseManager createDatabaseManager(String jdbcDriver, String url, String user, String password) throws ClassNotFoundException, SQLException{
+        if(manager == null) manager = new DatabaseManager(jdbcDriver, url, user, password); 
+        return manager;
+    }
     public void addPlayer(String login){
         try{
             PreparedStatement stat = conn.prepareStatement("INSERT INTO Statistics VALUES(?, 0, 0)");
@@ -22,5 +27,15 @@ public class DatabaseManager {
         }catch(SQLException e){
             Logger.getLogger(DatabaseManager.class.getName()).log(Level.SEVERE, null, e);
         }
+    }
+    public boolean existsPlayer(String login){
+        try(PreparedStatement stat = conn.prepareStatement("SELECT login FROM Statistics WHERE login = ?")){
+            stat.setString(1, login);
+            ResultSet rs = stat.executeQuery();
+            return rs.next();
+        }catch(SQLException e){
+            Logger.getLogger(DatabaseManager.class.getName()).log(Level.SEVERE, null, e);
+        }
+        return false;
     }
 }
