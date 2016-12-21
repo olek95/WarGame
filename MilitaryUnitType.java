66fb7da7413184de3854db7b0fed35b8766910e1
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-12-21 11:52:29 +0100

    Update MilitaryUnitType.java

--- MilitaryUnitType.java
+++ MilitaryUnitType.java
@@ -1,7 +1,13 @@
 package wargame;
 
+/**
+ * Typ wyliczeniowy <code>MilitaryUnitType</code> reprezentuje możliwe typy 
+ * jednostek występujących w grze. Są to: żołnierz, czołg, helikopter oraz baza.
+ * @author AleksanderSklorz
+ */
 public enum MilitaryUnitType {
     SOLDIER, 
     TANK,
-    HELICOPTER
+    HELICOPTER,
+    BASE
 }
