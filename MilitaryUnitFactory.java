eafc742a30dc1799a59d1e8914354414f7cfc8bb
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-12-21 11:52:01 +0100

    Update MilitaryUnitFactory.java

--- MilitaryUnitFactory.java
+++ MilitaryUnitFactory.java
@@ -1,6 +1,16 @@
 package wargame;
 
+/**
+ * Klasa <code>MilitaryUnitFactory</code> reprezentuje fabrykę jednostek, która
+ * tworzy jednostki danego typu. 
+ * @author AleksanderSklorz
+ */
 public class MilitaryUnitFactory {
+    /**
+     * Tworzy jednostkę podanego typu. 
+     * @param type typ jednostki
+     * @return jednostka podanego typu
+     */
     public static MilitaryUnit createMilitaryUnit(MilitaryUnitType type){
         if(MilitaryUnitType.SOLDIER.equals(type)) return new Soldier(); 
         if(MilitaryUnitType.TANK.equals(type)) return new Tank();
